4871ad67907d888b8ab3a6cfc33bdee9768bb990
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-05-13 19:08:09 +0000

    impl custom exceptions

---
+++ lib/core_utils/chat_exceptions.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+abstract class ChatExceptions extends Equatable implements Exception {
+  final String error;
+  const ChatExceptions(this.error);
+
+  @override
+  String toString() => '$runtimeType $error';
+
+  @override
+  List<Object?> get props => [error];
+
+}
+
+class CategoryCreationException extends ChatExceptions {
+  const CategoryCreationException(super.error);
+}
+
+class UserCreationException extends ChatExceptions {
+  const UserCreationException(super.error);
+}
+
+class UserInfoUpdateException extends ChatExceptions {
+  const UserInfoUpdateException(super.error);
+}
+
+class DataConnectionTimeout extends ChatExceptions {
+  const DataConnectionTimeout(super.error);
+}
