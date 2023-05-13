6b6ad6b0e3f48313855783aa6885ffe1dc1d1892
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-05-13 16:32:47 +0000

    setup chat page

---
+++ lib/chat/chat.dart
@@ -0,0 +1,2 @@
+export 'views/chat_page.dart';
+export 'bloc/chat_bloc.dart';
---
+++ lib/chat/views/chat_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  static String route = "/chat";
+
+  @override
+  Widget build(BuildContext context) {
+    return _ChatView();
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
