4e1a9e08e8039b20ad7962ad718e7d6ace6f0ab9
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-05-15 01:19:46 +0000

    cloud func() - deleteUser

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,22 @@
 const functions = require("firebase-functions");
 
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+const {onCall} = require("firebase-functions/v1/https");
+
+initializeApp();
+
+const firestore = getFirestore();
+
+// if window closed.. remove userId from users
+exports.deleteUserRecord = onCall((request)=> {
+  const uid = request.data.uid;
+  const collection = request.data.collection;
+  firestore.collection(collection).doc(uid).delete().then((ret) => {
+    functions.logger.info(ret);
+  });
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
